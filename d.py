4ddeab3159c761dc8b962f1d720467e86ef356c3
Adam Michael <adam@ajmichael.net>
2015-09-19 14:20:59 -0400

    Completed.

--- d.py
+++ d.py
@@ -1,19 +1,29 @@
-35
-10 5
-20 12
-8 13
--1 -1
-25
-10 5
-20 13
-3 12
--1 -1
-15
-5 17
-5 17
-5 17
-7 9
-7 20
-2 10
--1 -1
-0
+import sys
+def spaces(x):
+  return len(x.split(' ')) - 1
+data = [x.strip() for x in sys.stdin.readlines()]
+data = filter(lambda x: x != '-1 -1', data)
+i = 0
+solutions = []
+while i < len(data):
+  max_width = int(data[i])
+  i += 1
+  total_height = 0
+  largest_width = 0
+  current_width = 0
+  current_height = 0
+  while i < len(data) and spaces(data[i]) == 1:
+    rwidth, rheight = [int(x) for x in data[i].split(' ')]
+    if (current_width + rwidth > max_width):
+      largest_width = max(largest_width, current_width)
+      total_height += current_height
+      current_width = 0
+      current_height = 0
+    else:
+      current_height = max(current_height, rheight)
+      current_width += rwidth
+      i += 1
+  largest_width = max(largest_width, current_width)
+  total_height += current_height
+  solutions += [str(largest_width) + ' x ' + str(total_height)]
+print '\n'.join(solutions[:-1])
